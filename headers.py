95cce60d0008d38ae1246d60b1b66be784d3ddbc
Ammar Azman <88831990+Ammar-Azman@users.noreply.github.com>
2023-08-05 09:31:16 +0800

    Add utils, headers, requirements file

---
+++ headers.py
@@ -0,0 +1,19 @@
+class AllHeader:
+    robust_header = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Cache-Control": "max-age=0",
+    }
+    headers_list = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.83 Safari/537.36",
+    ]
---
+++ utils.py
@@ -0,0 +1,147 @@
+import time
+import random
+import json
+import requests
+import logging
+import concurrent.futures
+from xtractor.headers import AllHeader
+from bs4 import BeautifulSoup
+
+head = AllHeader()
+
+
+def request_page(link: str, delay_time: int = 2, use_random_header=False) -> object:
+    """
+    Given link, return html text extracted from website
+
+    :param link:str
+    :param use_random_header:Boolear = False
+
+    >>> link_harakah = "https://harakahdaily.net/index.php/2018/04/29/senarai-calon-bertanding-pru14/"
+    >>> assert type(request_text(link_harakah)) == str, "Not return string"
+
+    """
+    if use_random_header:
+        random_header = random.choice(head.headers_list)
+    else:
+        random_header = head.headers_list[0]
+
+    robust_header = head.robust_header
+    header_list = [{"User-Agent": random_header}, robust_header]
+
+    time.sleep(delay_time)
+    response = requests.get(link, headers=random.choice(header_list))
+    return response
+
+
+def get_text(response: object) -> str:
+    """
+    Given response from request_text, return html text
+
+    >>> assert type(get_text(response)) == str, "Function not return string"
+    """
+    html_text = response.text
+
+    assert type(html_text) == str, "request_text not return a string"
+    # print("TEXT:\n", html_text)
+    return html_text
+
+
+def bs4_soup(html_text: str) -> object:
+    """
+    Given html_text, return soup object
+    """
+    soup_ = BeautifulSoup(html_text, "lxml")
+    return soup_
+
+
+def get_href(soup: object) -> list:
+    """
+    Given soup object, return href list
+    """
+    div_tag = soup.find(
+        "div",
+        {"class": "wp-block-query alignwide has-global-padding is-layout-constrained"},
+    )
+    a_tag = div_tag.find_all("a", href=True)
+    href_list = [x["href"] for x in a_tag if "page" not in x["href"]]
+    href_list = list(set(href_list))
+
+    return href_list
+
+
+def extractor(link: str) -> object:
+    """
+    Given list of link, it will return a soup object
+    for html tag processing
+
+    :param link:str
+    """
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+    response = request_page(link)
+    logging.info(f"Link: {link} -- status {response.status_code}")
+
+    html_text = get_text(response)
+    soup_ = bs4_soup(html_text)
+    return soup_
+
+
+def multi_threading(
+    todo: object, links: list = False, links_dict: dict = False, worker_num=3
+) -> list:
+    """
+    Function that wraped the extractor function to apply
+    multithreading processing for faster extraction.
+
+    :param todo: object | function
+    :param links: list
+    :param links_dict: dict
+    :return final_output: list
+
+    """
+    final_output = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=worker_num) as executor:
+        if not links_dict:
+            text_html = {
+                executor.submit(todo, idx, url): url for idx, url in enumerate(links)
+            }
+        else:
+            text_html = {
+                executor.submit(todo, link): link
+                for link in links_dict["article_links"]
+            }
+        for future in concurrent.futures.as_completed(text_html):
+            # url = text_html[future]
+            try:
+                final_output.append(future.result())
+            except Exception as e:
+                print(f"Error: {e}")
+    return final_output
+
+
+def dumps_the_json(content: dict, json_file_name: str):
+    """
+    Given the content:dict and json_file_name,
+    return json file in the project path
+
+    :param content:dict
+    :param json_file_name:str
+    :return json file
+
+    """
+    with open(json_file_name, "w") as file:
+        json.dump(content, file)
+
+
+def read_the_json(json_file: str):
+    """
+    Given json_file, return the content
+    of the json_file
+
+    :param json_file:str
+    :return link_to_extract:list
+    """
+    with open(json_file, "r") as file:
+        link_to_extract = json.load(file)
+    return link_to_extract
